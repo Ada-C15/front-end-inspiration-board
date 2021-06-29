0b004d85fc61f88f2ed741a884d9c06aa0263d9a
Manu Ponce <kmanuponce@gmail.com>
2021-06-29 16:32:52 -0700

    Start of NewBoard component.

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+
 
 const NewBoardForm = (props) => {
-    return (
-        <form>
-            {/* Title input
+  const [title, setTitle] = useState("");
+  const [owner, setOwner] = useState("");
+
+  const newBoard = () => {
+    // alert(title);
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, {"title":title,"owner":owner})
+      .then((response) => {
+        console.log(
+          "The data we get back from the HTTP response:",
+          response.data
+        );
+      })
+      .catch((error) => {
+        console.log(
+          "Anything that isn't status code 2XX is an error:",
+          error.response.status
+        );
+        console.log(
+          "The data from response with an error:",
+          error.response.data
+        );
+      });
+  };
+
+  return (
+    <form>
+      {/* Title input
             Owner input
             Submit button */}
-        </form>
-    );
-
+      <input
+        name="title"
+        id="title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        name="owner"
+        id="owner"
+        value={owner}
+        onChange={(event) => setOwner(event.target.value)}
+      />
+      <button onClick={newBoard}>SUBMIT</button>
+    </form>
+  );
 };
 
 export default NewBoardForm;
