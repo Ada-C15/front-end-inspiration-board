1b5e8101141b493b9e0c3f2355f78c5cebbeea5d
Tammy <iptmhatuw@gmail.com>
2021-06-29 11:57:06 -0700

    update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,16 +5,30 @@ const Board = (props) => {
     // move this to parent file 
     const [boardTitle, setboardTitle] = useState("");
 
+    const displayBoardButtonClick = () => {
+        const displayBoard = {
+            title: props.title,
+        }
+    }
+    
     return (
-        <div className="board_div">
+        <div>
+        <div className="selectedBoard_div"></div>
+        <h2>SELECTED BOARD</h2>
+        <div>
+            <p onClick={displayBoardButtonClick}>{props.title}</p>
+        </div>
+        <div className="allBoards_div">
             <h2>BOARDS</h2>
             {/* How to increase index? */}
             <div>{indexIncrease}{boardTitle}</div>
         </div>
+        </div>
     );
 };
 
-export default Card;
+
+export default Board;
 
 
 const Student = (props) => {
