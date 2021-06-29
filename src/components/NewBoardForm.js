5ff5fd366b407d2770cee59904ec50fcb5fdadc7
Jen Lim <jennifer.lim07@gmail.com>
2021-06-29 11:28:04 -0700

    newboardform component and callback function working

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 
 /*NewBoardForm
 
@@ -8,3 +8,49 @@ createNewBoard
 State:
 title
 owner */
+
+const NewBoardForm = (props) => {
+    const [title, setTitle] = useState('');
+    const [ownerName, setOwnerName] = useState('');
+
+    const onTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const onOwnerNameChange = (event) => {
+        setOwnerName(event.target.value);
+    }
+
+    const onBoardSubmit = (event) => {
+        event.preventDefault();
+
+        //Work on callback function
+        props.createNewBoard({
+            title: title, 
+            ownerName: ownerName
+        })
+
+        //Reset fields
+        setTitle('');
+        setOwnerName('');
+    }
+
+    return (
+        <form onSubmit={onBoardSubmit}>
+            <label htmlFor="title-input">Title</label>
+            <input
+                name="title"
+                value={title}
+                onChange={onTitleChange}/>
+            <label htmlFor="owner-input">Owner's Name</label>
+            <input 
+                name="owner-name"
+                value={ownerName}
+                onChange={onOwnerNameChange}/>
+            <input type="submit" value="Submit"/>
+        </form>
+    )
+
+}
+
+export default NewBoardForm;
