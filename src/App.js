6fc8859e37f529e9fb29eb65985bc84fb2ff356e
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-06-29 23:17:41 -0700

    adds request to get board data and display selected board

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import BoardList from './components/BoardList';
 import './App.css';
+import Board from './components/Board';
 import NewBoardForm from './components/NewBoardForm';
-import './App.css';
 
-// Data Structure Model:
-//
-// BoardList = [
-//     {title: 'title',
-//       owner: 'owner',
-//       cards: [{message: 'something', likes: 0}, {message: 'something else', likes: 2}]
-//     },
-//     {title: 'title',
-//       owner: 'owner',
-//       cards: [card1, card2, card3]
-//     },
-//     {title: 'title',
-//       owner: 'owner',
-//       cards: [card1, card2, card3]
-//     }
-//       ]
-
-
+// getBoardData
 function App() {
+  //STATES:
+  const [boardData, setBoardData] = useState([]);
+  const [currentBoard, setCurrentBoard] = useState({});
+  
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`,
+    {
+      params: {
+      format: 'json'
+      }
+    })
+    .then( (response) => {
+      setBoardData(response.data);
+      console.log('success in finding boardList')
+    })
+    .catch( (error) => {
+      console.log('error in getting board list');
+      console.log(error.response)
+      alert("Could not connect to boards")
+    });
+  }, []);
+
+
+  const selectBoard = (event) => {
+    if (event.target.value) {
+      for (let board of boardData) {
+        if (event.target.value === board.title) {
+          setCurrentBoard(board);
+        }
+      }
+    } else {
+      setCurrentBoard({});
+    }
+  }
+
+
+  const handleChange = (event) => { 
+    selectBoard(event);
+  }
+
+
+  const generateBoardTitles = (boardData) => {
+    const boardTitles = [];
+    for (let board of boardData) {
+        boardTitles.push(<option key={board.id} value={board.title}>{board.title}</option>);
+    }
+    return boardTitles;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -31,10 +63,16 @@ function App() {
       </header>
       <main>
         <h3>Boards List:</h3>
-        <BoardList></BoardList>
-        <h3>Selected Board: </h3>
+        {/* Created this as a drop-down list, not sure if I like it */}
+        <select id="boards" onChange={handleChange} value={currentBoard.title}>
+          <option value=""></option>
+          {generateBoardTitles(boardData)}
+        </select>
+        <h3>Selected Board: {currentBoard.title}</h3>
         <h3>Create a New Board:</h3>
         <NewBoardForm></NewBoardForm>
+
+        <Board data={currentBoard}></Board>
       </main>
     </div>
   );
