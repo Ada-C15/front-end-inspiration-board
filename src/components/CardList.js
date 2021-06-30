9c8302192915f6e99c18f6b51a4deb38f7eb4c2d
Sidney DuPont <sidneyadupont@gmail.com>
2021-06-30 15:27:35 -0700

    Updates route for card display to match Flask API

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -7,18 +7,41 @@ const CardList = (props) => {
     const [cardsData, setCardsData] = useState([]);
 
     useEffect(() => {
-        axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}`)
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`)
         .then((response) => {
-            setCardsData(response.data.cards);
+            setCardsData(response.data);
             console.log(props)
         }).catch((error) => {
             console.log('Error:', error);
         })
     }, [props]);
 
+    const deleteCardItem = (card_id) => {
+        axios.delete(`${ process.env.REACT_APP_BACKEND_URL }/cards/${ card_id }`)
+            .then((response) => {
+                const newCardsData = cardsData.filter((existingCard) => {
+                    return existingCard.card_id !== card_id;
+                });
+                setCardsData(newCardsData);
+            })
+            .catch((error) => {
+                console.log("Error:", error);
+                alert("Couldn't delete the card.");
+            });
+    };
+
     const cardElements = cardsData.map((card) => {
-        return (<Card card_id={card.card_id} message={card.message} like_count={card.like_count}/>)
+        return (<Card card_id={card.card_id} message={card.message} like_count={card.like_count} deleteCardCallback={deleteCardItem}/>)
     });
+
+    const createCard = (message) => {
+      axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`, {message})
+      .then((response) => {
+        const cards = [...cardsData];
+        cards.push(response.data.card);
+        setCardsData(cards);
+      })
+    }
 
     return (
     <section className='cards__container'>
@@ -28,6 +51,7 @@ const CardList = (props) => {
           {cardElements}
         </div>
       </section>
+      <NewCardForm createCardCallback={createCard} />
     </section>)
 }
 
