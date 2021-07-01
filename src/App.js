366709e5ffaef4282de742f4556b4357d5cb080b
Ruthie Newman <ruthienewman@protonmail.com>
2021-06-30 19:28:47 -0700

    Adds Card, CardList and NewCardForm components to components folder. Populates Card, CardList and NewCardForm component files.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import CardList from './components/CardList.js';
+import Card from './components/Card.js';
 import NewBoardForm from './components/NewBoardForm';
+import NewCardForm from './components/NewCardForm';
+import axios from 'axios';
+
+
+//BLUEPRINTS: 
+//"/cards", "/<int:id>/cards", "/<int:id>", "/<int:id>/like"
+//"/boards"
+
 
 // process.env.REACT_APP_BACKEND_URL
 // axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
@@ -12,8 +22,117 @@ boardsData
 selectedBoard
 isBoardFormVisible */
 
+//Creates helper data array to test with
+const createCards = () => {
+  const cards = [];
+
+  let current_id = 0
+
+  for(let row = 0; row < 9; row +=1) {
+    cards.push({
+      card_id: current_id,
+      likes_count: 0,
+      message: 'This works!'});
+      current_id += 1
+  }  
+  console.log('cards', cards)
+  return cards;
+}
+
+
 function App() {
+  const BASE_URL = 'http://localhost:5000';
+
   const [boardsData, setBoardsData] = useState([])
+  const [cards, setCards] = useState(createCards()); //for test data
+  // const [cards, setCards] = useState([]);  //for API calls
+
+  //when a user clicks a card list then:
+
+  // useEffect(()=>{
+    //   axios
+    //   .get(`${BASE_URL}/cards`)
+    //   .then((response) => console.log(response))
+    //   //.then((response)=>setCards(response))//setCards will have an empty obj or list as default([])
+    //   .catch((error)=>console.log(error));
+    // }, []);
+    //use state to
+    //then it will trigger render so that it gets added to cardList with -setCards
+    //the way we trigger a re-render is by calling a set function
+
+  const createNewCard = (newCardObj) => {
+
+    console.log('hi im in createNewCard')
+    console.log(newCardObj.message)
+
+    // axios
+    //     .post(`${BASE_URL}/boards/{board_id}/cards/`, newCardObj)
+    //     //   .then((response) => console.log(response))
+    //     .then((response)=>{
+    //       // const newCardsList = [...cards]
+    //       // newCardsList.push(response.data)
+    //       //setCards(newCardsList)
+    //       setCards([...cards, response.data]) //do I need to specify here?
+    //     })
+    //     .catch((error)=>console.log(error));
+  
+    
+
+    //recieves an object from NewCardForm constructed from the data from the form fields : 
+    //{
+  //   card_id: Id,
+  //   likes_count: 0,
+  //   message: ''
+  //  }
+
+    //put card data into database with POST request
+    // useEffect(()=>{
+    //   axios
+    //   .post(`${BASE_URL}/cards`, {newCardObject.newMessage})
+    //   .then((response) => console.log(response))
+    //   //.then((response)=>setCards(response))//setCards will have an empty obj or list as default([])
+          //or .then((response)=>())
+    //   .catch((error)=>console.log(error));
+    // }, []);
+
+    // //then do API GET request to get all the cards and then put that data in setCards to update state
+    
+  } 
+
+    const testDeleteCardCallback = (card_id) => {
+      console.log('Im in delete')
+      axios.delete(`${BASE_URL}/cards/card_id`)
+      .then(() => ());
+    }
+
+    const testLikeCallback = (card_id) => {
+      console.log('Im in Like', card_id)
+
+      //still needs logic
+
+      // axios.patch(`${BASE_URL}/cards/card_id`)
+      //.then(setCards(response.data.card))
+      //.catch((error) => console.log(error))
+
+
+      // update state/iterate through cards and find the one I want and change the likes count
+      //then display the updated card
+      //check out update student
+    }
+
+    const testfunction = () => {
+      console.log('This is a test function')
+    }
+
+  return(
+    <div className="Cards">
+      <h2>Cards</h2>
+      <main>
+        <CardList cards={cards} onClickCallback={testfunction} deleteCallback={testDeleteCardCallback} likeCallback={testLikeCallback}/>
+        <NewCardForm createNewCard={createNewCard} />
+      </main>
+    </div>
+  )
 
   const CreateNewBoard = (newBoard) => {
     console.log('newBoard ', newBoard)
@@ -33,6 +152,7 @@ function App() {
       <NewBoardForm createNewBoard={CreateNewBoard}/>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/CardList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../App.css';
+import Card from './Card';
+import PropTypes from 'prop-types';
+
+
+const CardList = (props) => {
+
+    console.log('cardsList props', props.cards)
+    const cardComponents = props.cards.map((card, index)=>{
+        return(
+            <li key={index}>
+                <Card
+                    id={card.card_id}
+                    message={card.message}
+                    onClick={props.onClickCallback}
+                    likeCallback={props.likeCallback}
+                    deleteCallback={props.deleteCallback}>
+                </Card>
+            </li>  
+        ); 
+    });
+    return (
+        <section>
+            <ul>
+                {cardComponents}
+            </ul>
+        </section>
+    );
+};
+
+
+// CardList.propTypes = {
+//     cards: PropTypes.arrayOf(
+//         PropTypes.arrayOf(PropTypes.shape({
+//             id:PropTypes.number.isRequired,
+//             messsage: PropTypes.string.isRequired
+//         }))
+//     ),
+//     onClickCallback: PropTypes.func
+// };
+
+export default CardList;
+
+
